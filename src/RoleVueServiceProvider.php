55eee1013c376404c3dd1afc92222e4ed3b5d820
Mark Pemburn <mark@pemburn.com>
2021-05-08 09:15:06 -0400

    Add views publish to RoleVueServiceProvider

--- src/RoleVueServiceProvider.php
+++ src/RoleVueServiceProvider.php
@@ -8,8 +8,11 @@ class RoleVueServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'rolevue');
+        
         $this->publishes([
             __DIR__.'/../config/rolevue.php' => config_path('rolevue.php'),
+            __DIR__ . '/resources/views' => base_path('resources/views/vendor/rolevue')
         ], 'config');
 
         $this->app['router']->namespace('Mpemburn\\RoleVue\\Controllers')
@@ -18,6 +21,6 @@ class RoleVueServiceProvider extends ServiceProvider
                 $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
             });
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'rolevue');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'rolevue');
     }
 }
