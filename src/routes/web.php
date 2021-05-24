35665776e80efe23081f8f00311684d7170777f1
Mark Pemburn <mark@pemburn.com>
2021-05-24 16:18:14 -0400

    Point views in routes to copied Blade files.

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,13 +4,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/roles', function () {
-    return view('roles.index');
+    return view('rolevue::roles.index');
 })->name('roles');
 
 Route::get('/permissions', function () {
-    return view('permissions.index');
+    return view('rolevue::permissions.index');
 })->name('permissions');
 
 Route::get('/user_roles', function () {
-    return view('user-roles.index');
+    return view('rolevue::user-roles.index');
 })->name('user_roles');
